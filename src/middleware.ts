9df9bb54c025b24ab866971b5341b199d4ba243d
Jessy Hanifiah <jessyhanifiah@gmail.com>
2023-11-28 12:03:59 +0700

    feat: add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { decode } from 'next-auth/jwt';
+// import { decode } from 'next-auth/jwt';
 
 export default async function middleware(req: NextRequest) {
   const token = req.cookies.get('next-auth.session-token')?.value;
-  console.log(token);
-  const payload = await decode({
-    token,
-    secret: process.env.NEXTAUTH_SECRET as string,
-  });
 
-  if (req.nextUrl.pathname === '/admin' && payload?.role === 'ADMIN') {
-    console.log('You are admin');
+  // const payload = await decode({
+  //   token,
+  //   secret: process.env.NEXTAUTH_SECRET as string,
+  // });
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/', req.url));
   }
 
   return NextResponse.next();
